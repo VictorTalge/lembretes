aea0134ea4b6643050f3a35bba8dd97c64a5ae49
Victor <vt.talge@gmail.com>
2021-04-05 20:32:11 -0300

    Index

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+2 const app = express();
+3 app.get ('/lembretes', (req, res) => {
+4
+5 });
+6 app.put ('/lembretes', (req, res) => {
+7
+8 });
+9
+10 app.listen(4000, () => {
+11 console.log('Lembretes. Porta 4000');
+12 });
