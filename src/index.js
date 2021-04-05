e610351b34564508cbf5c3e1282d6708ff5a6dc4
Victor <vt.talge@gmail.com>
2021-04-05 20:37:51 -0300

    Segunda modificação

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.get('/lembretes', (req, res) => {
+
+});
+app.put('/lembretes', (req, res) => {
+
+});
+
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000');
+});
