73ef116183ef429f230bf1d64c3d09709eff306a
Victor <vt.talge@gmail.com>
2021-04-08 21:21:37 -0300

    Atualizado

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
-app.use(bodyParser.json());
-lembretes = {};
-contador = 0;
-app.get('/lembretes', (req, res) => {
-    res.send(lembretes);
-});
-app.put('/lembretes', (req, res) => {
-    contador++;
+app.use(express.json());
+const axios = require('axios');
+
+const observacoesPorLembreteId = {};
+const {
+    v4: uuidv4
+} = require('uuid');
+
+//:id é um placeholder
+//exemplo: /lembretes/123456/observacoes
+app.put('/lembretes/:id/observacoes', async (req, res) => {
+    const idObs = uuidv4();
     const {
         texto
     } = req.body;
-    lembretes[contador] = {
-        contador,
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete =
+        observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({
+        id: idObs,
         texto
-    }
-    res.status(201).send(lembretes[contador]);
+    });
+    observacoesPorLembreteId[req.params.id] =
+        observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs,
+            texto,
+            lembreteId: req.params.id
+        }
+    })
+    res.status(201).send(observacoesDoLembrete);
 });
-app.listen(4000, () => {
-    console.log('Lembretes. Porta 4000');
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || []);
+
 });
+app.listen(5000, (() => {
+    console.log('Lembretes  . Porta 5000');
+}));
